2a2b3853a4ff5dc83fca09faa6d61d19cff14b33
ELIZA <rysaeva96@mail.ru>
2022-07-03 19:18:17 +0500

    Решение задачи

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -4,3 +4,46 @@
 // массив можно ввести с клавиатуры, либо задать на старте
 // выполнения алгоритма
 
+string[] GenArray()
+{
+    Console.Write("Введите число элементов массива: ");
+    int n = int.Parse(Console.ReadLine());
+    string[] array = new string[n];
+    Console.Write($"Введите элементы массива из {n} символов: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void PrintArray(string[] prarr)
+{
+    var str = string.Join(" ", prarr);
+    Console.WriteLine(str);
+}
+
+string[] array = GenArray();
+Console.WriteLine("Исходный массив: ");
+PrintArray(array);
+
+string[] SampleArray(string[] array)
+{
+    string[] newarray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            newarray[i] = array[i];
+        }
+        else if (array[i].Length > 3)
+        {
+            array[i] = string.Empty;
+        }
+    }
+    return newarray;
+}
+
+string[] newarray = SampleArray(array);
+Console.WriteLine("Элементы массива с числом элементов равным трем и меньше: ");
+PrintArray(newarray);
